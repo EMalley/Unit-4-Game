0cdeaed0bd207816bb0c97838063b78709d1aa93
EMalley <EMalley998@gmail.com>
2018-11-25 17:01:10 -0500

    4boxesAdded

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,22 @@
-// load Javascript after document is ready
-    var randomResult;
-    var wins;
-    var lose;
+// execute this after DOM has fully loaded
+$(document).ready(function(){
+
+// load Javascript After Document is ready
+var randomResult;
+var wins;
+var lose;
+
+// Chooses Random Number to reach to win game. 
+randomResult = Math.floor(Math.random() * 100 + 20)
+$("#result").html('Number to Win: ' + randomResult);
+console.log(randomResult);
+
 
 // Set connection with 4 crystals and declare random result for a win
 for (var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() * 12);
+    var random = Math.floor(Math.random() * 12) + 1;
     console.log(random);
-
-    var crystal = $("<div>");
-        crystal.attr("class", "crystal");
-        console.log(crystal)
-
-    $(".crystals").append(crystal);
+$('.crystalArea').append('<div class="crystals"></div');
 
 };
 
@@ -22,3 +26,11 @@ for (var i = 0; i < 4; i++) {
 // set winning score between 19-120
 // check win === total score
 // check loose (goes over Score)
+
+// reset crystal/winning score after win or loose
+$("#newGameBtn").click(function (){
+    random = random;
+    randomResult = randomResult;
+});
+});
+
