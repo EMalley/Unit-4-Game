0366834ae482ad4662f9d4c6cb118f93de9992ea
EMalley <EMalley998@gmail.com>
2018-11-26 17:28:09 -0500

    working newGameBtn

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,48 @@
 // execute this after DOM has fully loaded
-$(document).ready(function(){
+$(document).ready(function () {
 
-// load Javascript After Document is ready
-var randomResult;
-var wins;
-var lose;
+    // load Javascript After Document is ready
+    var randomResult;
+    var wins;
+    var lose;
 
-// Chooses Random Number to reach to win game. 
-randomResult = Math.floor(Math.random() * 100 + 20)
-$("#result").html('Number to Win: ' + randomResult);
-console.log(randomResult);
+    // Chooses Random Number to reach to win game. between 19-120
+    randomResult = Math.floor(Math.random() * 100 + 20)
+    $("#result").html('Number To Win: ' + randomResult);
+    console.log(randomResult);
 
+    // Set connection with 4 crystals (between 1-12) and declare random result for a win
+    for (var i = 0; i < 4; i++) {
+        var random = Math.floor(Math.random() * 12) + 1;
+        console.log(random);
+        $('.crystalArea').append('<div class="crystals"></div');
+        // assign onclick data to each crystal box
 
-// Set connection with 4 crystals and declare random result for a win
-for (var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() * 12) + 1;
-    console.log(random);
-$('.crystalArea').append('<div class="crystals"></div');
+    };
+    console.log("--------------------------")
 
-};
+    // crystal score needs to add on to the previous score
 
-// each crystal needs a randomly generated Number between 1-12
-// crystal score needs to add on to the previous score
-// each new game, crystals should reset and be a new vaule between 1-12
-// set winning score between 19-120
-// check win === total score
-// check loose (goes over Score)
+    // each new game, crystals should reset and be a new vaule between 1-12
 
-// reset crystal/winning score after win or loose
-$("#newGameBtn").click(function (){
-    random = random;
-    randomResult = randomResult;
-});
+    // check win === total score
+
+    // check loose (goes over Score)
+
+    // reset crystal/winning score after win or loose
+
+    $("#newGameBtn").click(function () {
+        // reset crystals
+        for (var i = 0; i < 4; i++) {
+            var random = Math.floor(Math.random() * 12) + 1;
+            console.log(random);
+            $('.crystalArea').append('<div class="crystals"></div');
+        };
+        // reset Winning Number
+        randomResult = Math.floor(Math.random() * 100 + 20)
+        $("#result").html('Number To Win: ' + randomResult);
+        console.log(randomResult);
+        console.log("--------------------------")
+    });
 });
 
