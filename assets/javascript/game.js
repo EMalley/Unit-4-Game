e9293f5038a8ab20135b596bbef6a21b7c549c59
EMalley <EMalley998@gmail.com>
2018-11-30 16:51:11 -0500

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
     var currentScore = 0
     var wins = 0
     var lose = 0
+    var winSound = document.createElement("audio");
+    winSound.setAttribute("src", "assets/Sounds/TaDa.mp3");
 
     // Chooses Random Number to reach to win game. between 19-120
     randomResult = Math.floor(Math.random() * 100 + 20)
@@ -25,32 +27,38 @@ $(document).ready(function () {
 
     // assign onclick data to each crystal box
     $("#currentScore").html('Current Score: ' + currentScore)
+
     $('.crystals').click(function () {
         console.log($(this).attr('data-random'));
-        console.log("------------------------------")
+        console.log("------------------------------");
 
         // crystal score needs to add on to the previous score
-        currentScore = parseInt($(this).attr("data-random")) + currentScore
-        $("#currentScore").html("Current Score: " + currentScore);
+        if (currentScore < randomResult) {
+            currentScore = parseInt($(this).attr("data-random")) + currentScore
+            $("#currentScore").html("Current Score: " + currentScore);
+        }
         
-    // check win === total score
-    $("#wins").html("Wins: " + wins);
-    if (randomResult === currentScore) {
-        wins++;
-        alert("You Win!");
-    };
-    // check loose (goes over Score)
-    $("#losses").html("Losses: " + lose);
-    if (currentScore > randomResult) {
-        lose++;
-        alert("Too bad, So sad! You loose!");
-    };
+        // check win === total score
+        $("#wins").html("Wins: " + wins);
+        if (randomResult === currentScore) {
+            $("#currentScore").html("Current Score: " + currentScore);
+            wins++;
+            winSound.play();
+            alert("You Win!");
+        };
+        // check loose (goes over Score)
+        $("#losses").html("Losses: " + lose);
+        if (currentScore > randomResult) {
+            lose++;
+            alert("Too bad, So sad! You loose!");
+        };
 
     });
 
     // each new game, crystals should reset and be a new vaule between 1-12
     $("#newGameBtn").click(function () {
         // reset crystals
+        currentScore = 0
         for (var i = 0; i < 4; i++) {
             var random = Math.floor(Math.random() * 12) + 1;
             console.log(random);
@@ -68,8 +76,11 @@ $(document).ready(function () {
 
         // set Current Score To 0
         $("#newGameBtn").click(function () {
-            currentScore = 0           
+            currentScore = 0
         });
         $("#currentScore").html("Current Score: 0")
     });
 });
+
+
+// $("ID").hide() and .show()
